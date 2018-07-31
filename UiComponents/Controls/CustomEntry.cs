3315d44561939c0e617386904dd25e06c5e656b1
Bruno Alexandre Apparecido <brunoapparecido@gmail.com>
2018-07-31 08:08:33 -0300

    - Custom Entry

---
+++ UiComponents/Controls/CustomEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Plugin.UiComponents.Controls
+{
+    public class CustomEntry : Entry
+    {
+
+        public static readonly BindableProperty BorderColorProperty =
+            BindableProperty.Create("BorderColor",
+                typeof(Color),
+                typeof(object),
+                Color.Black);
+
+        public Color BorderColor
+        {
+            get { return (Color)GetValue(BorderColorProperty); }
+            set { SetValue(BorderColorProperty, value); }
+        }
+
+        public static readonly BindableProperty CornerRadiusProperty =
+            BindableProperty.Create("CornerRadius",
+                typeof(float),
+                typeof(object),
+                default(float));
+
+        public float CornerRadius
+        {
+            get { return (float)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+    }
+}
---
+++ UiComponents/Platforms/Android/CustomEntryRenderer.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Plugin.UiComponents.Controls;
+using Plugin.UiComponents.Platforms.Android;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace Plugin.UiComponents.Platforms.Android
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        private GradientDrawable _gradientDrawable;
+
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+            _gradientDrawable = new GradientDrawable();
+            _gradientDrawable.SetShape(ShapeType.Rectangle);
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.SetPadding(PaddingLeft + 30, PaddingTop + 30, PaddingRight + 30, PaddingBottom + 30);
+
+            UpdateBorderColor();
+            UpdateCornerRadius();
+            UpdateBackground();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == CustomEntry.BorderColorProperty.PropertyName)
+                UpdateBorderColor();
+            if (e.PropertyName == CustomEntry.CornerRadiusProperty.PropertyName)
+                UpdateCornerRadius();
+
+            UpdateBackground();
+        }
+
+        protected override void UpdateBackgroundColor()
+        {
+           // base.UpdateBackgroundColor();
+
+            _gradientDrawable.SetColor(Element.BackgroundColor.ToAndroid());
+        }
+
+        private void UpdateBorderColor()
+        {
+            _gradientDrawable.SetStroke(3, ((CustomEntry)Element).BorderColor.ToAndroid());
+        }
+
+        private void UpdateCornerRadius()
+        {
+            _gradientDrawable.SetCornerRadius(((CustomEntry)Element).CornerRadius);
+        }
+
+        private void UpdateBackground()
+        {
+            Control.SetBackground(_gradientDrawable);
+        }
+
+        
+
+       
+    }
+}
